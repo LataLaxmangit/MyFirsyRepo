83e64635dff2ee55234fb49d87c2ef2d43c1a51f
LataLaxmangit <95337258+LataLaxmangit@users.noreply.github.com>
2021-12-01 18:14:22 +0530

    Update Branchclass2.java

--- LntJavaProject/src/Branchclass2.java
+++ LntJavaProject/src/Branchclass2.java
@@ -3,7 +3,7 @@ public class Branchclass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("");
+System.out.println("added to branch");
 	}
 
 }
